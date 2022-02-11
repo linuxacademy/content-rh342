6de627a3c0af2c11dedc615f22580aa2cef7383c
Elle <elle.krout@acloud.guru>
2022-02-11 13:52:50 -0500

    PCP lab

---
+++ labs/section-2/use-performance-co-pilot/scripts/init.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+# move files
+mkdir /opt/scripts/
+mv /tmp/files/labs/section-2/use-performance-co-pilot/scripts/logger.sh /opt/scripts/logger.sh
+mv /tmp/files/labs/section-2/use-performance-co-pilot/scripts/logger.service /etc/systemd/service/logger.service
+
+# pcp setup
+yum install -y pcp pcp-system-tools pmlogger
+systemctl start pmcd pmlogger
+systemctl enable pmcd pmlogger
+
+# start cpukiller
+systemctl start logger
+systemctl enable logger
