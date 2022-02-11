1939667b8e929e756df882df630d0eed92293f03
Elle <elle.krout@acloud.guru>
2022-02-11 14:42:03 -0500

    PCP init script fix. again.

--- labs/section-2/use-performance-co-pilot/scripts/init.sh
+++ labs/section-2/use-performance-co-pilot/scripts/init.sh
@@ -3,7 +3,7 @@
 # move files
 mkdir /opt/scripts/
 mv /tmp/files/labs/section-2/use-performance-co-pilot/scripts/logger.sh /opt/scripts/logger.sh
-mv /tmp/files/labs/section-2/use-performance-co-pilot/scripts/logger.service /etc/systemd/service/logger.service
+mv /tmp/files/labs/section-2/use-performance-co-pilot/scripts/logger.service /etc/systemd/system/logger.service
 
 # pcp setup
 yum install -y pcp pcp-system-tools 
