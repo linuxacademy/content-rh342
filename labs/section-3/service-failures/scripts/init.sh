e6085e6f0f0c46a003cd57b1efb8fd533ab07ea3
Elle <elle.krout@acloud.guru>
2022-03-24 15:28:07 -0400

    Service Failures init update

--- labs/section-3/service-failures/scripts/init.sh
+++ labs/section-3/service-failures/scripts/init.sh
@@ -2,5 +2,7 @@
 
 mv /tmp/files/labs/section-3/service-failures/grafana/grafana.repo /etc/yum.repos.d/grafana.repo
 yum install -y grafana pcp pcp-system-tools
+mkdir /etc/systemd/system/grafana.service.d/
+mkdir /etc/systemd/system/pmcd.service.d/
 mv /tmp/files/labs/section-3/service-failures/dependencies/grafana /etc/systemd/system/grafana.service.d/depends.conf
 mv /tmp/files/labs/section-3/service-failures/dependencies/pmcd /etc/systemd/system/pmcd.service.d/depends.conf
